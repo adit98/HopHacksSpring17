0811758ad0b1500fc20f59ce8919de13304057ad
Richard Chen <rchen40@jhu.edu>
2017-02-19 05:27:57 -0500

    asdfsadf

--- testAlexa.py
+++ testAlexa.py
@@ -3,6 +3,16 @@ from flask_ask import Ask, statement, question, session
 import json
 import requests
 import time
+from collections import deque
+path = deque()
+
+from datetime import datetime
+curr_date = datetime.now().strftime('%Y-%m-%d').split("-")
+months_dict = {1:"January", 2:"February", 3:"March", 4:"April", 5:"May", 6:"June", 7:"July", 8:"August", 9:"September", 10:"October", 11:"November", 12:"December"}
+curr_date = months_dict[int(curr_date[1])] + " " + curr_date[2] + ", " + curr_date[0]
+ 
+import numpy as np
+history_illness_db = np.load("history.db.npy")
 
 app = Flask(__name__)
 
@@ -14,28 +24,48 @@ def homepage():
 
 @ask.launch
 def welcome():
-	msg = "Hi there, sorry to hear you aren't feeling well. What do you need?"
+	path.appendleft("home")
+	msg = "Hi there, I am Alexa Medic. How are you? Are you feeling sick?"
+	msg = "OK. Next"
 	return question(msg)
 
 @ask.intent("NeedIntent")
-def DecisionTree(need):
-	if need == "sick":
-		msg = "Ah, so I see you are sick. Please list out your symptoms, one by one."
+def decisionTree(need):
+	if need == "history":
+		path.append("history")
+		msg = "Would you like your complete history?"
 		return question(msg)
-	elif need == "insurance":
+	if need == "sick": # nlp
+		path.append("diagnosis")
+		msg = "Ah, so I see you are sick. Would you like a diagnosis?"
+		msg = "OK. Next"
+		return question(msg)
+	elif need == "insurance": # nlp
 		msg = "OK, let me consult your medical insurance provider. Ah, so I see you have Obama care. Unfortunately, the ACA was repealed as of January 5th, 2017. I'm tilted."
 		return statement(msg)
 
 @ask.intent('YesIntent')
-def askForSymptoms():
-	msg = "Please list out your symptoms, one by one."
-	return question(msg)
+def yesIntent():
+	print path[-1]
+	if path[-1] == "history":
+		msg = "Here is the medical history I have recorded:"
+		for incidence in history_illness_db:
+			msg += incidence[0] + ", " + incidence[1] + ". "
+		return question(msg)
+	elif path[-1] == "diagnosis":
+		print "yes"
+		msg = "Please list out your symptoms, one by one, and I will try to help."
+		msg = "OK. Next"
+		return question(msg)
 
-@ask.intent('ProcessIntent')
+@ask.intent('SymptomIntent')
 def processSymptoms(symptom):
+	global history_illness_db 
 	print symptom
-	msg = "Your symptoms are ... Nausea ... Dizziness ... Chest Pain"
-	return statement(msg)
+	msg = "Your symptoms are. " + symptom + ". What else do you need?"
+	history_illness_db = np.vstack((history_illness_db, np.array([curr_date, symptom])))
+	np.save("history.db.npy", history_illness_db)
+	return question(msg)
 
 
 @ask.intent('NoIntent')
